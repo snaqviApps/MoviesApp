3a573c82f68ab5ca5aa49372d1a7a4fc020c9a1e
sayyidnaqvi <naqvi.sdsu@gmail.com>
2026-08-31 05:00:31 -0500

    fixed post configuration-change repetition of power-status notification

--- app/src/main/java/edu/review/moviesappreview/presentation/screen/system/PowerStatusScreen.kt
+++ app/src/main/java/edu/review/moviesappreview/presentation/screen/system/PowerStatusScreen.kt
@@ -5,8 +5,11 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import edu.review.moviesappreview.domain.PowerState
 import edu.review.moviesappreview.presentation.viewmodel.PowerViewModel
@@ -19,9 +22,13 @@ const val CHANNEL_TITLE = "Power Status Notifications"
 fun PowerStatusScreen(
     viewModel: PowerViewModel = hiltViewModel()
 ) {
-    val context = LocalContext.current
-    val powerState by viewModel.powerState.collectAsStateWithLifecycle()
     val channelId = "power_status_channel"
+    val context = LocalContext.current
+
+    val powerState by viewModel.powerState.collectAsStateWithLifecycle()
+
+    /** to track configuration changes, observed by Compose Snapshot System  */
+    var lastNotifiedState by rememberSaveable { mutableStateOf<String?>(null) }
 
     val requestPermissionLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission()
@@ -30,19 +37,28 @@ fun PowerStatusScreen(
     }
 
     LaunchedEffect(powerState) {
-        val message = when (powerState) {
-            is PowerState.PluggedStatusLoading -> return@LaunchedEffect
+        val currentStateKey = when (powerState) {
+            is PowerState.PluggedStatusLoading -> null  // Loading is ignored
             is PowerState.PluggedIn -> "🔌 Cable plugged in real-time!"
             is PowerState.PluggedOut -> "🔋 Cable pulled out real-time!"
         }
 
-        createNotificationChannelsAndSetupNotifications(
-            context = context,
-            channelId = channelId,
-            message = message,
-            requestPermissionLauncher = requestPermissionLauncher,
-            contentTitle = CHANNEL_TITLE,
-            notificationId = POWER_NOTIFICATION_ID
-        )
+        /**
+         * lastNotifiedState: String? is serving as a "switch", to notify the user
+         * only if in configuration changes, the notification has already not been sent,
+         * here for Power-States
+         */
+        if(currentStateKey != null  && (currentStateKey != lastNotifiedState)) {
+                createNotificationChannelsAndSetupNotifications(
+                    context = context,
+                    channelId = channelId,
+                    message = currentStateKey,
+                    requestPermissionLauncher = requestPermissionLauncher,
+                    contentTitle = CHANNEL_TITLE,
+                    notificationId = POWER_NOTIFICATION_ID
+                )
+                lastNotifiedState = currentStateKey     // update the lastest notification-state
+            }
+
     }
 }
